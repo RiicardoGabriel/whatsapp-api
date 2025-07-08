d69c38e9c9398c184dae9ac6036d26389f789cca
Ricardo Gabriel <97189467+RiicardoGabriel@users.noreply.github.com>
2025-07-08 12:20:42 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,20 +7,11 @@ const { maxAttachmentSize } = require('./config')
 const cors = require('cors')
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || origin === 'http://localhost:5173' || /https:\/\/.*\.sweetpdv\.com\.br$/.test(origin) || origin === 'https://sweetpdv.com.br') {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  methods: 'GET, POST, PUT, DELETE, OPTIONS',
-  allowedHeaders: 'Content-Type, Authorization, X-Api-Key',
-  credentials: true
-}))
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE'
+}));
 
-// Middleware para resolver o problema de "Preflight Request" (OPTIONS)
-app.options('*', cors())
+app.use(bodyParser.json({ limit: 'Infinity' }));
 
 // Initialize Express app
 app.disable('x-powered-by')
